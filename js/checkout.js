10903ce1ce82cbdcb91b4453e972ad00ce26023c
wavesforweirdos <wavesforweirdos@gmail.com>
2023-03-06 19:08:30 +0100

    all functions implemented

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,81 @@
+const form = document.getElementById("form");
+// Array with input fields
+const arrayForm = [{
+		'type': 'text',
+		'inputID': 'fName',
+	},
+	{
+		'type': 'text',
+		'inputID': 'fLastN',
+	},
+	{
+		'type': 'number',
+		'inputID': 'fPhone',
+	},
+	{
+		'type': 'mix',
+		'inputID': 'fPassword',
+	},
+	{
+		'type': 'email',
+		'inputID': 'fEmail',
+	},
+	{
+		'type': 'mix',
+		'inputID': 'fAddress',
+	},
+]
 
-// Exercise 6
-function validate() {
-	var error = 0;
+arrayForm.map(input => {
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	input.fieldElement = document.getElementById(input.inputID);
+})
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
+function show_validation_message(fieldElement) {
+	fieldElement.style.display = 'block';
+	fieldElement.classList.add("is-invalid");
+}
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
+function hide_validation_message(fieldElement) {
+	fieldElement.classList.remove("is-invalid");
+	fieldElement.classList.add("is-valid");
+}
 
+function validate_empty_input_and_lenght_of_field(input) {
+	//All fields are required & min 3 digits
+	if (input.value == "" || input.value.length < 3) {
+		return true;
+	} else {
+		return false
+	}
+}
+
+function validate_data_type(type, input) {
+	//Validates type of data 
+	if (type.includes('text')) {
+		pattern = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+	} else if (type.includes('number')) {
+		pattern = /^\d+$/gi;
+	} else if (type.includes('email')) {
+		pattern = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+	} else if (type.includes('mix')) {
+		pattern = /^[A-Za-z0-9]{8,12}$/;
+	}
+	return pattern.test(input.value);
+
+}
+
+function validate() {
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		var error = 0;
+
+		arrayForm.map(input => {
+			if (validate_empty_input_and_lenght_of_field(input.fieldElement) || !validate_data_type(input.type, input.fieldElement)) {
+				show_validation_message(input.fieldElement)
+			} else {
+				hide_validation_message(input.fieldElement)
+			}
+		})
+	}, false)
 }
--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,5 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-   {
+var products = [{
         id: 1,
         name: 'cooking oil',
         price: 10.5,
@@ -74,33 +73,142 @@ var total = 0;
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for (let index = 0; index < products.length; index++) {
+        // 2. Add found product to the cartList array
+        if (products[index].id == id) {
+            cartList.push(products[index]);
+        }
+    }
+
+    printCountProduct();
+}
+
+function printCountProduct() {
+    let countProduct = document.getElementById('count_product');
+    countProduct.innerHTML = cartList.length;
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList = [];
+    cart = [];
+    open_modal();
+    printCountProduct();
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    total = 0;
+    for (let index = 0; index < cart.length; index++) {
+        total += cart[index].subtotalWithDiscount;
+    }
+
+    return total;
 }
 
 // Exercise 4
+function countRepeatedElements(array, element) {
+    let counter = 0;
+    for (let index = 0; index < array.length; index++) {
+        for (item of array) {
+            if (item === element) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+
+    //Restauramos el array CART para prevenir errores
+    cart = [];
+
+    //Guardamos los valores únicos, sin repeticiones, de la CARTLIST
+    let uniqueCartList = [...new Set(cartList)];
+
+    for (let index = 0; index < uniqueCartList.length; index++) {
+        //Por cada elemento del array UNIQUECARTLIST contaremos la cantidad de veces que se repite en la CARLIST 
+        let quantity = 0;
+        quantity = countRepeatedElements(cartList, uniqueCartList[index]);
+
+        //y la añadiremos como atributo en CART
+        const product = {
+            name: uniqueCartList[index].name,
+            price: uniqueCartList[index].price,
+            type: uniqueCartList[index].type,
+            quantity: quantity,
+            subtotal: uniqueCartList[index].price * quantity,
+            subtotalWithDiscount: uniqueCartList[index].price * quantity,
+        };
+        cart.push(product);
+    }
 }
 
 // Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+
+    for (let index = 0; index < cart.length; index++) {
+        for (item of cart) {
+            if (item.name == "cooking oil" && item.quantity > 2) {
+                let price = 10;
+                item.subtotalWithDiscount = price * item.quantity;
+            }
+
+            if (item.name == "Instant cupcake mixture" && item.quantity >= 10) {
+                let price = item.price * 2 / 3;
+                item.subtotalWithDiscount = price * item.quantity;
+            }
+        }
+    }
 }
 
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    let tbody = document.getElementById('cart_list');;
+    tbody.innerHTML = '';
+    // Crea las celdas
+    for (let i = 0; i < cart.length; i++) {
+
+        // Crea las hileras de la tabla
+        const tr = document.createElement('tr');
+
+        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+        // texto sea el contenido de <td>, ubica el elemento <td> al final
+        // de la hilera de la tabla
+        const th = document.createElement('th');
+        const textoTh = document.createTextNode(cart[i].name);
+        th.appendChild(textoTh);
+        tr.appendChild(th);
+
+        const tdPrice = document.createElement('td');
+        const textoTdPrice = document.createTextNode("$" + cart[i].price);
+        tdPrice.appendChild(textoTdPrice);
+        tr.appendChild(tdPrice);
+
+        const tdQty = document.createElement('td');
+        const textoTdQty = document.createTextNode(cart[i].quantity);
+        tdQty.appendChild(textoTdQty);
+        tr.appendChild(tdQty);
+
+        const tdTotal = document.createElement('td');
+        const textoTdTotal = document.createTextNode("$" + cart[i].subtotalWithDiscount);
+        tdTotal.appendChild(textoTdTotal);
+        tr.appendChild(tdTotal);
+
+        // agrega la hilera al final de la tabla (al final del elemento tbody)
+        tbody.appendChild(tr);
+    }
+
+    let totalPrice = calculateTotal();
+    let totalPricePrinted = document.getElementById('total_price');
+
+    totalPricePrinted.innerHTML = totalPrice;
 }
 
 
@@ -109,17 +217,24 @@ function printCart() {
 // Exercise 7
 function addToCart(id) {
     // Refactor previous code in order to simplify it 
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    buy(id);
+    generateCart();
+    applyPromotionsCart();
 }
 
 // Exercise 8
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for (let index = 0; index < cartList.length; index++) {
+        if (cartList[index].id == id) {
+            // 2. Remove found product to the cartList array
+            cartList.splice(index, 1);
+            generateCart();
+            return applyPromotionsCart();
+        }
+    }
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal() {
+    printCart();
 }
